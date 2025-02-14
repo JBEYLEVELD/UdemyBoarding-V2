48d71860950052c7c55ca9622fc0e2ede3a3f572
Jeremy Beyleveld <jbeyleveld@singular.co.za>
2025-02-14 15:01:58 +0200

    End of Section 6

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,14 +1,12 @@
 import { Box } from "@mui/material";
 import ActivityCard from "./ActivityCard";
-
 type Props = {
     activities: Activity[]
     selectActivity: (id: string) => void;
-    deleteActivity: (id: string) => void;
 }
 
 
-export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
+export default function ActivityList({ activities, selectActivity }: Props) {
     return (
         <Box sx={{ display: 'flex', gap: 3, flexDirection: 'column' }}>
             {activities.map(activity => (
@@ -16,7 +14,6 @@ export default function ActivityList({ activities, selectActivity, deleteActivit
                     key={activity.id}
                     activity={activity}
                     selectActivity={selectActivity}
-                    deleteActivity={deleteActivity}
                 />
             ))}
 
